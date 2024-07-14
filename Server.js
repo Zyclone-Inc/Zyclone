d959257460be49ff93d30ba5cfa19900d96869c3
Zyclone Inc. <zyclone.inc@gmail.com>
2024-07-14 17:30:32 +0200

    Structure
    First files uploaded to website.

---
+++ Server.js
@@ -0,0 +1,9 @@
+// Backend Server
+
+const ZycloneTitle = document.getElementById(`ZycloneTitle`);
+
+ZycloneTitle.textContent = `Zyclone Inc.`;
+
+console.log(`Server/Website label is: ${ZycloneTitle.textContent}`);
+
+// Co-Database System
